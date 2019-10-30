7a72a5f43c89a89cc614bd69189d9856e0f0fd46
BranceA <brancealexander@gmail.com>
2019-10-29 20:24:05 -0500

    basic search functionality done

--- src/main/java/com/codeup/adlister/controllers/CounterServlet.java
+++ src/main/java/com/codeup/adlister/controllers/CounterServlet.java
@@ -1,12 +1,16 @@
 package com.codeup.adlister.controllers;
 
 import com.codeup.adlister.dao.DaoFactory;
+import com.codeup.adlister.models.Ad;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @WebServlet(name = "controllers.CounterServlet", urlPatterns = "/count")
 public class CounterServlet extends HttpServlet {
@@ -14,7 +18,18 @@ public class CounterServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String userSearch = request.getParameter("search");
-        request.setAttribute("ads", DaoFactory.getAdsDao().all());
-        request.getRequestDispatcher("/WEB-INF/temp-search.jsp");
+        List<Ad> ads = DaoFactory.getAdsDao().all();
+        List<Ad> searchedAds = new ArrayList<>();
+        for(Ad ad : ads){
+            if(ad.getTitle().toLowerCase().contains(userSearch.toLowerCase())){
+                searchedAds.add(ad);
+            }
+        }
+        request.setAttribute("ads", searchedAds);
+        try {
+            request.getRequestDispatcher("/WEB-INF/temp-search.jsp").forward(request, response);
+        } catch (ServletException e) {
+            e.printStackTrace();
+        }
     }
 }

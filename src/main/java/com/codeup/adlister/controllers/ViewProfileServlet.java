773c3044e7b9ac589b4b17fbd10447a822756bd5
BranceA <brancealexander@gmail.com>
2019-10-30 10:42:31 -0500

    users ads are directed to their profile page

--- src/main/java/com/codeup/adlister/controllers/ViewProfileServlet.java
+++ src/main/java/com/codeup/adlister/controllers/ViewProfileServlet.java
@@ -1,11 +1,17 @@
 package com.codeup.adlister.controllers;
 
+import com.codeup.adlister.dao.DaoFactory;
+import com.codeup.adlister.models.Ad;
+import com.codeup.adlister.models.User;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @WebServlet(name = "controllers.ViewProfileServlet", urlPatterns = "/profile")
 public class ViewProfileServlet extends HttpServlet {
@@ -14,6 +20,21 @@ public class ViewProfileServlet extends HttpServlet {
             response.sendRedirect("/login");
             return;
         }
-        request.getRequestDispatcher("/WEB-INF/profile.jsp").forward(request, response);
+
+        User user = (User) request.getSession().getAttribute("user");
+        List<Ad> ads = DaoFactory.getAdsDao().all();
+        List<Ad> userAds = new ArrayList<>();
+        System.out.println(user.getId());
+        for(Ad ad : ads){
+            if(user.getId() == ad.getUserId()){
+                userAds.add(ad);
+            }
+        }
+        request.setAttribute("userAds", userAds);
+        try {
+            request.getRequestDispatcher("/WEB-INF/profile.jsp").forward(request, response);
+        } catch (ServletException e) {
+            e.printStackTrace();
+        }
     }
 }
